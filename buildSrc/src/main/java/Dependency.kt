f8e1180bcab24f97851bb66d4a29db16c5255c35
sigirisaicharan <sai.sigiri@dunzo.in>
2023-09-19 11:20:08 +0530

    added buildSrc for handling dependency's

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,34 @@
+object Version {
+    const val KOTLIN = "1.8.21"
+    const val MIN_SDK_VERSION = 23
+    const val TARGET_SDK_VERSION = 33
+
+    const val COMPILE_SDK_VERSION = 33
+    const val APP_VERSION_CODE = 3000
+    const val APP_VERSION_NAME = "0.0.0.1"
+}
+
+object AndroidXMiscellaneous {
+    const val CORE_KTX = "androidx.core:core-ktx:1.10.1"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
+    const val ACTIVITY_COMPAT = "androidx.activity:activity-ktx:1.4.0"
+    const val FRAGMENT_COMPAT = "androidx.fragment:fragment-ktx:1.5.7"
+}
+
+object GoogleAndroid {
+    const val MATERIAL_DESIGN = "com.google.android.material:material:1.9.0"
+}
+
+object AndroidXUI {
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.3.0"
+    const val CARD_VIEW = "androidx.cardview:cardview:1.0.0"
+    const val DYNAMIC_ANIMATION = "androidx.dynamicanimation:dynamicanimation:1.0.0"
+}
+
+object AndroidXTesting {
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.5"
+    const val JUNIT = "junit:junit:4.13.2"
+    const val EXT_JUNIT_KTX = "androidx.test.ext:junit-ktx:1.1.5"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
+}
