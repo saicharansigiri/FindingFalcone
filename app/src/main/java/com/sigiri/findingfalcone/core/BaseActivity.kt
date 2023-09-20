2639f2e0e29fbba56a38aba3597f515ecf6cc89d
sigirisaicharan <sai.sigiri@dunzo.in>
2023-09-20 15:09:51 +0530

    setting content view

--- app/src/main/java/com/sigiri/findingfalcone/core/BaseActivity.kt
+++ app/src/main/java/com/sigiri/findingfalcone/core/BaseActivity.kt
@@ -3,6 +3,7 @@ package com.sigiri.findingfalcone.core
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
+import com.sigiri.findingfalcone.R
 
 abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
 
@@ -13,5 +14,6 @@ abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = setUpViewBinding()
+        setContentView(binding.root)
     }
 }
